6d293d9b63e04584e1970f008efd69e533240efd
MisanthropicBit <albo.developer@gmail.com>
2023-12-17 22:17:37 +0100

    Tests for not wrapping full width/height windows

--- tests/wrap_around_spec.lua
+++ tests/wrap_around_spec.lua
@@ -8,6 +8,8 @@ local given = vader.given
 local make_layout = test_helpers.make_layout
 
 describe("wrap-around when moving windows", function()
+    assert:set_parameter("TableFormatLevel", 10)
+
     it("wraps around when enabled in the config", function()
         config.configure({
             at_edge = {
@@ -92,6 +94,130 @@ describe("wrap-around when moving windows", function()
                     { "leaf", main_win_id },
                     { "leaf" },
                 },
+            })
+        end)
+    end)
+
+    it("does not wrap full width window", function()
+        config.configure({
+            at_edge = {
+                horizontal = at_edge.Wrap,
+                vertical = at_edge.Wrap,
+            },
+            keymaps = {
+                move = {
+                    split_left = "sh",
+                    split_down = "sj",
+                    split_up = "sk",
+                    split_right = "sl",
+                },
+            },
+        })
+
+        given("", function()
+            local main_win_id = make_layout({
+                "col",
+                {
+                    "main",
+                    {
+                        "row",
+                        { "leaf", "leaf" },
+                    },
+                }
+            })["main"]
+
+            assert.matches_winlayout(vim.fn.winlayout(), {
+                "col",
+                {
+                    { "leaf", main_win_id },
+                    {
+                        "row",
+                        {
+                            { "leaf" },
+                            { "leaf" },
+                        },
+                    },
+                },
+            })
+
+            vim.api.nvim_set_current_win(main_win_id)
+            winmove.move_window(main_win_id, "h")
+            winmove.move_window(main_win_id, "l")
+
+            assert.matches_winlayout(vim.fn.winlayout(), {
+                "col",
+                {
+                    { "leaf", main_win_id },
+                    {
+                        "row",
+                        {
+                            { "leaf" },
+                            { "leaf" },
+                        },
+                    },
+                }
+            })
+        end)
+    end)
+
+    it("does not wrap full height window", function()
+        config.configure({
+            at_edge = {
+                horizontal = at_edge.Wrap,
+                vertical = at_edge.Wrap,
+            },
+            keymaps = {
+                move = {
+                    split_left = "sh",
+                    split_down = "sj",
+                    split_up = "sk",
+                    split_right = "sl",
+                },
+            },
+        })
+
+        given("", function()
+            local main_win_id = make_layout({
+                "row",
+                {
+                    "main",
+                    {
+                        "col",
+                        { "leaf", "leaf" },
+                    }
+                }
+            })["main"]
+
+            assert.matches_winlayout(vim.fn.winlayout(), {
+                "row",
+                {
+                    { "leaf", main_win_id },
+                    {
+                        "col",
+                        {
+                            { "leaf" },
+                            { "leaf" },
+                        },
+                    }
+                }
+            })
+
+            vim.api.nvim_set_current_win(main_win_id)
+            winmove.move_window(main_win_id, "k")
+            winmove.move_window(main_win_id, "j")
+
+            assert.matches_winlayout(vim.fn.winlayout(), {
+                "row",
+                {
+                    { "leaf", main_win_id },
+                    {
+                        "col",
+                        {
+                            { "leaf" },
+                            { "leaf" },
+                        },
+                    }
+                }
             })
         end)
     end)
