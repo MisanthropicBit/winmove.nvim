afc9d6cd43377e541c072b286e610f216856f8ee
MisanthropicBit <albo.developer@gmail.com>
2024-01-09 21:14:16 +0100

    Full dimension tests across tabs

--- tests/move_between_tabs_spec.lua
+++ tests/move_between_tabs_spec.lua
@@ -419,6 +419,280 @@ describe("moving between tabs", function()
         end)
     end)
 
+    it("splits full width window across tabs", function()
+        given("", function()
+            local source_win_ids = make_layout({
+                "col",
+                {
+                    "main",
+                    {
+                        "row",
+                        {
+                            "old_left",
+                            "old_right",
+                        },
+                    },
+                },
+            })
+
+            vim.cmd.tabnew()
+
+            local target_win_ids = make_layout({
+                "row",
+                {
+                    "left",
+                    "right",
+                },
+            })
+
+            local win_id = source_win_ids["main"]
+            vim.api.nvim_set_current_win(win_id)
+            local before_buffer = vim.api.nvim_get_current_buf()
+
+            winmove.split_into(win_id, "l")
+            local after_buffer = vim.api.nvim_get_current_buf()
+
+            -- Window ids are not the same but the buffer is
+            assert.are.same(before_buffer, after_buffer)
+
+            assert.matches_winlayout(vim.fn.winlayout(1), {
+                "row",
+                {
+                    { "leaf", source_win_ids["old_left"] },
+                    { "leaf", source_win_ids["old_right"] },
+                },
+            })
+
+            assert.matches_winlayout(vim.fn.winlayout(2), {
+                "row",
+                {
+                    { "leaf" },
+                    { "leaf", target_win_ids["left"] },
+                    { "leaf", target_win_ids["right"] },
+                },
+            })
+        end)
+    end)
+
+    it("splits full height window across tabs", function()
+        given("", function()
+            local target_win_ids = make_layout({
+                "col",
+                {
+                    "top",
+                    "bottom",
+                },
+            })
+
+            vim.cmd.tabnew()
+
+            local source_win_ids = make_layout({
+                "row",
+                {
+                    "main",
+                    {
+                        "col",
+                        {
+                            "old_top",
+                            "old_bottom",
+                        },
+                    },
+                },
+            })
+
+            local win_id = source_win_ids["main"]
+            vim.api.nvim_set_current_win(win_id)
+            local before_buffer = vim.api.nvim_get_current_buf()
+
+            winmove.split_into(win_id, "h")
+            local after_buffer = vim.api.nvim_get_current_buf()
+
+            -- Window ids are not the same but the buffer is
+            assert.are.same(before_buffer, after_buffer)
+
+            assert.matches_winlayout(vim.fn.winlayout(1), {
+                "col",
+                {
+                    {
+                        "row",
+                        {
+                            { "leaf", target_win_ids["top"] },
+                            { "leaf" },
+                        },
+                    },
+                    { "leaf", target_win_ids["bottom"] },
+                },
+            })
+
+            assert.matches_winlayout(vim.fn.winlayout(2), {
+                "col",
+                {
+                    { "leaf", source_win_ids["old_top"] },
+                    { "leaf", source_win_ids["old_bottom"] },
+                },
+            })
+        end)
+    end)
+
+    it("moves full width window", function()
+        given("", function()
+            local source_win_ids = make_layout({
+                "col",
+                {
+                    "main",
+                    {
+                        "row",
+                        {
+                            "old_left",
+                            "old_right",
+                        },
+                    },
+                },
+            })
+
+            vim.cmd.tabnew()
+
+            local target_win_ids = make_layout({
+                "row",
+                {
+                    "left",
+                    "right",
+                },
+            })
+
+            local win_id = source_win_ids["main"]
+            vim.api.nvim_set_current_win(win_id)
+            local before_buffer = vim.api.nvim_get_current_buf()
+
+            winmove.move_window(win_id, "l")
+            local after_buffer = vim.api.nvim_get_current_buf()
+
+            -- Window ids are not the same but the buffer is
+            assert.are.same(before_buffer, after_buffer)
+
+            -- Check window layout of tab 1
+            assert.matches_winlayout(vim.fn.winlayout(1), {
+                "row",
+                {
+                    { "leaf", source_win_ids["old_left"] },
+                    { "leaf", source_win_ids["old_right"] },
+                },
+            })
+
+            -- Check window layout of tab 2
+            assert.matches_winlayout(vim.fn.winlayout(2), {
+                "row",
+                {
+                    {
+                        "col",
+                        {
+                            { "leaf" },
+                            { "leaf", target_win_ids["left"] },
+                        },
+                    },
+                    { "leaf", target_win_ids["right"] },
+                },
+            })
+        end)
+    end)
+
+    it("moves full height window", function()
+        given("", function()
+            local source_win_ids = make_layout({
+                "row",
+                {
+                    {
+                        "col",
+                        {
+                            "old_top",
+                            "old_bottom",
+                        },
+                    },
+                    "main",
+                },
+            })
+
+            vim.cmd.tabnew()
+
+            local target_win_ids = make_layout({
+                "col",
+                {
+                    "top",
+                    "bottom",
+                },
+            })
+
+            local win_id = source_win_ids["main"]
+            vim.api.nvim_set_current_win(win_id)
+            local before_buffer = vim.api.nvim_get_current_buf()
+
+            winmove.move_window(win_id, "l")
+            local after_buffer = vim.api.nvim_get_current_buf()
+
+            -- Window ids are not the same but the buffer is
+            assert.are.same(before_buffer, after_buffer)
+
+            -- Check window layout of tab 1
+            assert.matches_winlayout(vim.fn.winlayout(1), {
+                "col",
+                {
+                    { "leaf", source_win_ids["old_top"] },
+                    { "leaf", source_win_ids["old_bottom"] },
+                },
+            })
+
+            -- Check window layout of tab 2
+            assert.matches_winlayout(vim.fn.winlayout(2), {
+                "col",
+                {
+                    { "leaf" },
+                    { "leaf", target_win_ids["top"] },
+                    { "leaf", target_win_ids["bottom"] },
+                },
+            })
+        end)
+    end)
+
+    it("closes tab if moving single window", function()
+        given("", function()
+            local target_win_ids = make_layout({
+                "row",
+                {
+                    "left",
+                    "right",
+                },
+            })
+
+            vim.cmd.tabnew()
+
+            local win_id = make_layout("main")["main"]
+            vim.api.nvim_set_current_win(win_id)
+            local before_buffer = vim.api.nvim_get_current_buf()
+
+            winmove.move_window(win_id, "h")
+            local after_buffer = vim.api.nvim_get_current_buf()
+
+            -- Window ids are not the same but the buffer is
+            assert.are.same(before_buffer, after_buffer)
+
+            assert.are.same(vim.fn.tabpagenr("$"), 1)
+
+            assert.matches_winlayout(vim.fn.winlayout(1), {
+                "row",
+                {
+                    { "leaf", target_win_ids["left"] },
+                    {
+                        "col",
+                        {
+                            { "leaf" },
+                            { "leaf", target_win_ids["right"] },
+                        },
+                    },
+                },
+            })
+        end)
+    end)
+
     it("does not move window if there is only one window and one tab", function()
         given("", function()
             stub(message, "error")
