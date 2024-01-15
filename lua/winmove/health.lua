a0f9a50507c484c6eb5f7d63325d083b06b2dfae
MisanthropicBit <albo.developer@gmail.com>
2024-01-15 21:14:48 +0100

    Update healthcheck neovim version

--- lua/winmove/health.lua
+++ lua/winmove/health.lua
@@ -2,21 +2,23 @@ local health = {}
 
 local config = require("winmove.config")
 
+local min_neovim_version = "0.8.0"
+
 function health.check()
     vim.health.report_start("winmove")
 
-    if vim.fn.has("nvim-0.5.0") == 1 then
-        vim.health.report_ok("Has neovim 0.5.0+")
+    if vim.fn.has("nvim-" .. min_neovim_version) == 1 then
+        vim.health.report_ok(("has neovim %s+"):format(min_neovim_version))
     else
-        vim.health.report_error("winmove.nvim requires at least neovim 0.5.0")
+        vim.health.report_error("winmove.nvim requires at least neovim " .. min_neovim_version)
     end
 
     local ok, error = config.validate(config)
 
-    if not ok then
-        vim.health.report_error("config has errors: " .. error)
-    else
+    if ok then
         vim.health.report_ok("found no errors in config")
+    else
+        vim.health.report_error("config has errors: " .. error)
     end
 end
 
