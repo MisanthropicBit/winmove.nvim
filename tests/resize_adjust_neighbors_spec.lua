0ec671e5677ca261d8cb63ac1c6477a56f4b11f6
MisanthropicBit <proximity.dev@proton.me>
2024-05-05 18:04:34 +0200

    Split resize tests into separate files

---
+++ tests/resize_adjust_neighbors_spec.lua
@@ -0,0 +1,11 @@
+local winmove = require("winmove")
+local vader = require("winmove.util.vader")
+local test_helpers = require("winmove.util.test_helpers")
+
+local given = vader.given
+local make_layout = test_helpers.make_layout
+
+describe("adjusts neighbors", function()
+    local count = 3
+    assert:set_parameter("TableFormatLevel", 10)
+end)
