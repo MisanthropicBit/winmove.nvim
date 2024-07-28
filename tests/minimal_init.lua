fc498424cf47732c8d2412fd7ed82db77d428818
MisanthropicBit <proximity.dev@proton.me>
2024-07-28 16:43:27 +0200

    Clean up minimal_init.lua

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,9 +1,3 @@
 vim.opt.rtp:append(".")
 vim.opt.rtp:append("~/.vim-plug/plenary.nvim")
 vim.cmd.runtime({ "plugin/plenary.vim", bang = true })
-vim.cmd.runtime({ "plugin/winmove.lua", bang = false })
-
--- vim.opt.rtp:append("~/.vim-plug/plenary.nvim")
--- vim.opt.rtp:append("~/.vim-plug/nvim-nio")
--- vim.opt.rtp:append("~/.vim-plug/neotest")
--- vim.opt.rtp:append("../neotest-busted")
