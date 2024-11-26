be440d3acfa69e354bb77724eafbb81738c55d4f
MisanthropicBit <proximity.dev@proton.me>
2024-11-26 20:11:51 +0100

    Fix test for v0.9.*

--- tests/highlight_spec.lua
+++ tests/highlight_spec.lua
@@ -1,4 +1,5 @@
 local winmove = require("winmove")
+local compat = require("winmove.compat")
 local config = require("winmove.config")
 
 describe("highlight", function()
@@ -17,12 +18,18 @@ describe("highlight", function()
         vim.cmd.vnew()
 
         local hl_group = "WinmoveMoveInternalType"
-        assert.are.same(#vim.api.nvim_get_hl(0, { name = hl_group, create = false }), 0)
+        local opts = { name = hl_group }
+
+        if compat.has("nvim-0.10.0") then
+            opts.create = false
+        end
+
+        assert.are.same(#vim.api.nvim_get_hl(0, opts), 0)
 
         winmove.start_mode(winmove.Mode.Move)
         winmove.stop_mode()
 
-        assert.are.same(vim.api.nvim_get_hl(0, { name = hl_group, create = false }), {
+        assert.are.same(vim.api.nvim_get_hl(0, opts), {
             bold = true,
             cterm = {
                 bold = true,
