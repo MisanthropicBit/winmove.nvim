f8a7a03cb4c65d257b470af7bead8622fa4d6bb8
MisanthropicBit <proximity.dev@proton.me>
2024-11-28 10:16:21 +0100

    Remove healthcheck test
    Requires additional setup to work on all versions.

--- tests/healthcheck_spec.lua
+++
@@ -1,7 +0,0 @@
-local health = require("winmove.health")
-
-describe("healthcheck", function()
-    it("runs healthcheck without failing", function()
-        assert.has_no_error(health.check)
-    end)
-end)
