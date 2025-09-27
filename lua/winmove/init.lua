0a98acf6c5007dcb286f9e31de0aca231b244927
MisanthropicBit <proximity.dev@proton.me>
2025-09-27 21:40:16 +0200

    Bump to v1.0.0 (#43)

--- lua/winmove/init.lua
+++ lua/winmove/init.lua
@@ -20,7 +20,7 @@ winmove.AtEdge = at_edge.AtEdge
 winmove.ResizeAnchor = resize.anchor
 
 local api = vim.api
-local winmove_version = "0.2.2"
+local winmove_version = "1.0.0"
 
 local augroup = api.nvim_create_augroup("Winmove", { clear = true })
 local autocmds = {}
